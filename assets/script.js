023b1b6278c586856ad25e9ccabf98fddd4978bc
sanford_paul00 <sanford.paul00@proton.me>
2025-05-19 19:27:23 -0700

    clean up contacts, move css and script to asset folder, ignoring rawAssets

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+// Shorthand for image elements
+const powerBIImage = document.getElementById("powerBIImage");
+const powerAppImage = document.getElementById("powerAppImage");
+const webDevImage = document.getElementById("webDevImage");
+
+// Function definition to enlarge image 
+const enlargeImage = (event) => {
+    event.target.style.transform = "scale(1.2)";
+    event.target.style.transition = "transform 0.5s ease";
+    event.target.style.width = "45%"
+    event.target.style.zIndex = 1000;
+};
+// Function to reset image to original size
+const resetImage = (event) => {
+    event.target.style.width = "25%";
+    event.target.style.transform = "scale(1)";
+    event.target.style.zIndex = 1;
+};
+
+
+// Add event listener for mouseenter of hero images
+powerBIImage.addEventListener("mouseenter", enlargeImage);
+powerBIImage.addEventListener("mouseleave", resetImage);
+
+powerAppImage.addEventListener("mouseenter", enlargeImage);
+powerAppImage.addEventListener("mouseleave", resetImage);
+
+webDevImage.addEventListener("mouseenter", enlargeImage);
+webDevImage.addEventListener("mouseleave", resetImage);
