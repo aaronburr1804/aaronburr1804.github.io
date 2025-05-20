9239f38c1829599b6a9ef50c5f4f64b92d8117c7
sanford_paul00 <sanford.paul00@proton.me>
2025-05-20 14:14:03 -0700

    disabiling hover animation on mobile devices

--- assets/script.js
+++ assets/script.js
@@ -5,15 +5,22 @@ const webDevImage = document.getElementById("webDevImage");
 
 // Function definition to enlarge image 
 const enlargeImage = (event) => {
-    event.target.style.transform = "scale(1.2)";
+    if (window.innerWidth < 600) {
+        
+    }
+    else {
     event.target.style.transition = "transform 0.5s ease";
     event.target.style.width = "45%"
     event.target.style.zIndex = 1000;
+    }
 };
 // Function to reset image to original size
 const resetImage = (event) => {
-    event.target.style.width = "25%";
-    event.target.style.transform = "scale(1)";
+    if (window.innerWidth < 600) {
+    }
+    else {
+        event.target.style.width = "25%";
+    }
     event.target.style.zIndex = 1;
 };
 
